8a08256f95d377fde66d9258bf1e513eea1afaf7
José Neto <josenetopr5@gmail.com>
2021-02-07 20:17:55 -0300

    TypeORM repository

--- back-end/src/database/migrations/1612731918674-CreateAppointments.ts
+++ back-end/src/database/migrations/1612731918674-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1612731918674
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
