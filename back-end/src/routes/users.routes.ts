c81a5e3c0233d0ac7cdad4c67d25ee1e8c3442b3
José Neto <josenetopr5@gmail.com>
2021-02-09 11:21:05 -0300

    Updating avatar

--- back-end/src/routes/users.routes.ts
+++ back-end/src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticade from '../middlewares/ensureAuthenticated';
 
@@ -35,7 +36,21 @@ usersRouter.patch(
   ensureAuthenticade,
   upload.single('avatar'),
   async (request, response) => {
-    return response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      // @ts-expect-error typescript version 4.0 breaking changes rules
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
