96ef5067b5e7b56513320f72c9e215e12ce0853f
José Neto <josenetopr5@gmail.com>
2021-02-09 10:16:05 -0300

    Authenticating routes

---
+++ back-end/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'ca3f19c8a97885e3686f5306a53901aa',
+    expiresIn: '1d',
+  },
+};
--- back-end/src/routes/appointments.routes.ts
+++ back-end/src/routes/appointments.routes.ts
@@ -5,7 +5,11 @@ import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
+import ensureAuthenticade from '../middlewares/ensureAuthenticated';
+
 const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticade);
 
 appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
