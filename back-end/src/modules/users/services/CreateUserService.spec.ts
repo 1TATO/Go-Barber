d8fd70d9f96f62ca458ef037c67db102f8d54de7
José Neto <josenetopr5@gmail.com>
2021-02-16 15:26:40 -0300

    Update avatar test

--- back-end/src/modules/users/services/CreateUserService.spec.ts
+++ back-end/src/modules/users/services/CreateUserService.spec.ts
@@ -1,13 +1,14 @@
 import AppError from '@shared/errors/AppError';
 
-import FakeusersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import CreateUserService from './CreateUserService';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
-    const fakeusersRepository = new FakeusersRepository();
+    const fakeusersRepository = new FakeUsersRepository();
     const fakeHashProvider = new FakeHashProvider();
+
     const createUser = new CreateUserService(
       fakeusersRepository,
       fakeHashProvider,
@@ -23,8 +24,9 @@ describe('CreateUser', () => {
   });
 
   it('should not be able to create a new user with same email from another', async () => {
-    const fakeusersRepository = new FakeusersRepository();
+    const fakeusersRepository = new FakeUsersRepository();
     const fakeHashProvider = new FakeHashProvider();
+
     const createUser = new CreateUserService(
       fakeusersRepository,
       fakeHashProvider,
