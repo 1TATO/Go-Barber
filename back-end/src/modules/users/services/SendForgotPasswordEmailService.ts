9f419f26ac6e4a516482501991856150eb2e3f3b
José Neto <josenetopr5@gmail.com>
2021-02-17 14:34:12 -0300

    Send email service in dev environment

--- back-end/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ back-end/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,10 +1,10 @@
 import { injectable, inject } from 'tsyringe';
 
 // import AppError from '@shared/errors/AppError';
-import IUsersRepository from '../repositories/IUsersRepository';
-import IUserTokensRepository from '../repositories/IUserTokensRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 // import User from '../infra/typeorm/entities/User';
 
@@ -32,9 +32,12 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists');
     }
 
-    await this.userTokensRepository.generate(user.id);
+    const { token } = await this.userTokensRepository.generate(user.id);
 
-    this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido');
+    await this.mailProvider.sendMail(
+      email,
+      `Pedido de recuperação de senha recebido: ${token}`,
+    );
   }
 }
 
