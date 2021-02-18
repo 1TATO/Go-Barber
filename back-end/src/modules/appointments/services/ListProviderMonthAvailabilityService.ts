6fc3139e2c2fcfcf73d1138daae82348c9713f4f
José Neto <josenetopr5@gmail.com>
2021-02-17 23:39:58 -0300

    Listing available days:

--- back-end/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ back-end/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,8 +1,7 @@
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
-
-// import User from '@modules/users/infra/typeorm/entities/User';
 
 interface IRequest {
   provider_id: string;
@@ -27,7 +26,7 @@ class ListProvidersMonthAvailabilityService {
     year,
     month,
   }: IRequest): Promise<IResponse> {
-    const appointments = this.appointmentsRepository.findAllInMonthFromProvider(
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
       {
         provider_id,
         year,
@@ -35,9 +34,25 @@ class ListProvidersMonthAvailabilityService {
       },
     );
 
-    console.log(appointments);
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-    return [{ day: 1, available: false }];
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
