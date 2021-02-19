17b12fe642adfb9e90180ef3611d57c3f455e646
José Neto <josenetopr5@gmail.com>
2021-02-19 10:51:02 -0300

    Amazon SES Emails

---
+++ back-end/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'josenetopr5@gmail.com',
+      name: 'José Neto',
+    },
+  },
+} as IMailConfig;
