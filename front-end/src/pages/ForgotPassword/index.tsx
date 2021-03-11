4c5a25c589d85e926c09dcf1c0df9110f15ac778
José Neto <josenetopr5@gmail.com>
2021-03-11 17:37:25 -0300

    Fixing link route to homepage in forgotPassword

--- front-end/src/pages/ForgotPassword/index.tsx
+++ front-end/src/pages/ForgotPassword/index.tsx
@@ -26,7 +26,6 @@ const ForgotPassword: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { addToast } = useToast();
-  // const history = useHistory();
 
   const handleSubmit = useCallback(
     async (data: ForgotPasswordFormData) => {
@@ -93,7 +92,7 @@ const ForgotPassword: React.FC = () => {
             </Button>
           </Form>
 
-          <Link to="/signin">
+          <Link to="/">
             <FiLogIn />
             Voltar ao login
           </Link>
